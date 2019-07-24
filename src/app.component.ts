8337ea02240d623c25144886fd45c1d0d6d9f3b8
adam.kulasiak <adam.kulasiak@gmail.com>
2019-07-24 21:52:35 +0200

    enevt binding

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'converter',
-  template: `<input id="baseAmountText" type="number" [value]="baseAmount"> USD =
+  template: `<input id="baseAmountText" type="number" [value]="baseAmount" (input)="update($event.target.value)"> USD =
             <strong id="targetAmountText" [innerText]="targetAmount"></strong> GBP`,
   styles: [
     `input[type=number] {
@@ -12,6 +12,12 @@ import { Component } from '@angular/core';
   ]
 })
 export class AppComponent {
+  exchangeRate = 0.70;
   baseAmount = 1;
-  targetAmount = 0.70;
+  targetAmount = this.exchangeRate;
+
+  update(baseAmount) {
+    this.targetAmount = parseFloat(baseAmount) * this.exchangeRate;
+
+  }
 }
